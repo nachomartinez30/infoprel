d73a72710f08ea76b218dce9984cf72ec190a10f
Ignacio <nachomartinez3010@gmail.com>
2020-02-28 10:52:20 -0600

    form de apooyos y ruta creada

---
+++ src/context/wizard/wizardState.jsx
@@ -0,0 +1,1 @@
+/* TODO: completar state para steper */
---
+++ src/rutas/Apoyos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Apoyos = () => {
+    return (
+        <h1>Apoyos</h1>
+    );
+}
+
+export default Apoyos;
