c4cdcf4cd71e68196773c688bfd019560d85fcfa
Ignacio <nachomartinez3010@gmail.com>
2020-02-28 10:51:17 -0600

    componente linea divisoria creado

---
+++ src/componentes/LineaDivision.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LineaDivision = () => {
+    return (
+        <div className="col-md-12">
+            <hr className="red" />
+        </div>
+    );
+}
+
+export default LineaDivision;
